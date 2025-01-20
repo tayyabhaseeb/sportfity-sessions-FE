1e9034d27b952b36e5b5cd26db5c50c628c78dce
“Tayyab <tayyabhaseeb5@gmail.com>
2025-01-20 12:48:02 +0000

    apis file added

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,6 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <h1 class="text-center text-red-500">
-      Template for the project with every thing installed and configured in it
-      already included tailwind css
-    </h1>
-  </header>
+  <Header />
   <RouterView />
 </template>
