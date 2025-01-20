26afb1f4c5690e76e5304d4015f2e70fec5b378e
Arian-Y <arianyousefi04@gmail.com>
2025-01-20 18:44:48 +0000

    little setup

--- src/Components/MatchDetails.vue
+++ src/Components/MatchDetails.vue
@@ -3,12 +3,6 @@
 </template>
 
 <script>
-import { getMatches } from "../src/utils/api";
 import { ref, watch, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import MatchCard from "./MatchCard.vue";
-
-export default {
-  components: MatchCard,
-};
 </script>
