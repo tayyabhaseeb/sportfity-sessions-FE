4e1b202deac9ece5a8c4bb5fc9393216fbe82604
“Tayyab <tayyabhaseeb5@gmail.com>
2025-01-20 10:21:10 +0000

    routing-comfigure

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import SecondPage from "../views/SecondPage.vue";
 
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: HomePage,
+    },
+    {
+      path: "/second",
+      component: SecondPage,
+    },
+  ],
+});
+
+export default router;
